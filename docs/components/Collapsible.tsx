3568405914d5c59c127a970a83555cc1922a1cb0
Matt Heisig <matt@hitfactormedia.com>
2025-09-02 11:15:16 -0700

    Fix callouts and code block styles

--- docs/components/Collapsible.tsx
+++ docs/components/Collapsible.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import * as React from "react";
 
 export default function Card({ children }: { children: React.ReactNode }) {
   const childrenArray = React.Children.toArray(children);
@@ -12,16 +12,32 @@ export default function Card({ children }: { children: React.ReactNode }) {
       {headerContent}
       {isOpen && bodyContent}
 
-      <div className="flex justify-center hover:cursor-pointer hover:bg-highlight py-3 transition-colors duration-300"
+      <div
+        className="flex justify-center hover:cursor-pointer hover:bg-highlight py-3 transition-colors duration-300"
         onClick={() => {
           setIsOpen((isOpen) => !isOpen);
-        }}>
-        
-        <div className={`transition-transform duration-300 ${isOpen ? '-rotate-90' : 'rotate-90'}`} >
-          <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1 11L6 6L1 1" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+        }}
+      >
+        <div
+          className={`transition-transform duration-300 ${isOpen ? "-rotate-90" : "rotate-90"}`}
+        >
+          <svg
+            width="7"
+            height="12"
+            viewBox="0 0 7 12"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M1 11L6 6L1 1"
+              stroke="currentColor"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
           </svg>
         </div>
       </div>
     </div>
-  );}
+  );
+}
